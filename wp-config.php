b45dca3020ec05409663f111315b994dbe4e03b9
Chris Nawoichik <47064228+chrisnawoichik@users.noreply.github.com>
2023-12-10 13:12:05 -0600

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,6 @@
+/** The home and site URLs */
+define('WP_SITEURL','https://julysecurities.com');
+define('WP_HOME','https://julysecurities.com');
+
+/** Set number of post revisions to keep */
+define('WP_POST_REVISIONS', 3);
